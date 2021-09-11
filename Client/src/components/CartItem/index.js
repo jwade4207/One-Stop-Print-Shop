3e93e0d56f0441aa1df9f31c4df9614eadac01a4
JJLindsey <jlindsey010@gmail.com>
2021-09-11 14:14:10 -0700

    add cart functionality & update globalState

--- Client/src/components/CartItem/index.js
+++ Client/src/components/CartItem/index.js
@@ -1,6 +1,38 @@
 import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
+//gets data as props passed down from Cart & updates the global state to adjust item quantity
 const CartItem = ({ item }) => {
+
+    const [, dispatch] = useStoreContext();
+    //cart item does not need to read state
+
+    const removeFromCart = item => {
+        dispatch({
+            type: REMOVE_FROM_CART,
+            _id: item._id
+        });
+    };
+
+    //edit quantitty of shopping cart items
+    const onChange = (e) => {
+        const value = e.target.value;
+
+        if (value === '0') {
+            dispatch({
+                type: REMOVE_FROM_CART,
+                _id: item._id
+            });
+        } else {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: item._id,
+                purchaseQuantity: parseInt(value)
+            });
+        }
+    };
+
     return (
         <div className="flex-row">
             <div>
@@ -17,13 +49,15 @@ const CartItem = ({ item }) => {
                         type="number"
                         placeholder="1"
                         value={item.purchaseQuantity}
+                        onChange={onChange}
                     />
                     <span
                         role="img"
                         aria-label="trash"
+                        onClick={() => removeFromCart(item)}
                     >
                         🗑️
-          </span>
+                    </span>
                 </div>
             </div>
         </div>
---
+++ Client/src/pages/Detail.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { useStoreContext } from "../utils/GlobalState";
+import { QUERY_BANNERS } from "../utils/queries";
+import spinner from "../assets/spinner.gif";
+import Cart from "../components/Cart";
+import {
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    ADD_TO_CART,
+    UPDATE_BANNERS,
+} from "../utils/actions";
+
+function Detail() {
+    const [state, dispatch] = useStoreContext();
+    const { id } = useParams();
+
+    const [currentBanner, setCurrentBanner] = useState({});
+
+    const { loading, data } = useQuery(QUERY_BANNERS);
+
+    const { banners, cart } = state;
+
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+        if (itemInCart) {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                banner: { ...currentBanner, purchaseQuantity: 1 },
+            });
+        }
+    };
+
+    const removeFromCart = () => {
+        dispatch({
+            type: REMOVE_FROM_CART,
+            _id: currentBanner._id,
+        });
+    };
+
+    useEffect(() => {
+        if (banners.length) {
+            setCurrentBanner(banners.find((banner) => banner._id === id));
+        } else if (data) {
+            dispatch({
+                type: UPDATE_BANNERS,
+                banners: data.banners,
+            });
+        }
+    }, [banners, data, dispatch, id]);
+
+    return (
+        <>
+            {currentBanner ? (
+                <div className="container my-1">
+                    <Link to="/">← Back to Banners</Link>
+
+                    <h2>{currentBanner.name}</h2>
+
+                    <p>{currentBanner.description}</p>
+
+                    <p>
+                        <strong>Price:</strong>${currentBanner.price}{" "}
+                        <button>onClick={addToCart}</button>
+                        <button
+                            disabled={!cart.find((p) => p._id === currentBanner._id)}
+                            onClick={removeFromCart}
+                        >
+                            Remove from Cart
+                        </button>
+                    </p>
+
+                    <img
+                        src={`/images/${currentBanner.image}`}
+                        alt={currentBanner.name}
+                    />
+                </div>
+            ) : null}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+        </>
+    );
+}
+
+export default Detail;
