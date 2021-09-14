9fff125e64a22de327403e412553c283dd9cd3ab
JJLindsey <jlindsey010@gmail.com>
2021-09-14 16:03:55 -0700

    update cart function w/queryCheckout & submit checkout

--- Client/src/components/Cart/index.js
+++ Client/src/components/Cart/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState.js';
 import { TOGGLE_CART } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { useLazyQuery } from '@apollo/client';
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -19,6 +34,20 @@ const Cart = () => {
             sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
     }
 
     if (!state.cartOpen) {
@@ -46,7 +75,7 @@ const Cart = () => {
 
                     {
                         Auth.loggedIn() ?
-                            <button>
+                            <button onClick={submitCheckout}>
                                 Checkout
                             </button>
                             :
