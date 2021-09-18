30438d97b52db0291d5910280d63212e68512028
CodyConck <codyconck@gmail.com>
2021-09-18 11:56:49 -0500

    updated index js in models, removed size

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Order = require('./Order');
 const Banner = require('./Banner');
-const Size = require('./Size');
+//const Size = require('./Size');
 
-module.exports = { User, Order, Banner, Size };
+module.exports = { User, Order, Banner };
